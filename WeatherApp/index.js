f7ae1e42da10363126bc3b8fef37063b39792a4a
hpPreetham1 <preetham.hp04@gmail.com>
2024-01-19 14:18:46 +0530

    final commit

---
+++ WeatherApp/index.js
@@ -0,0 +1,45 @@
+const apiKey = "a73333f60c6a335c7a91fafaed9d5fe0";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const  searchBox = document.querySelector(".search input");
+const  searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl+ city +`&appid=${apiKey}`);
+
+    if(response.status == 404){
+    document.querySelector(".weather").style.display="none"
+    document.querySelector(".error").style.display="block"
+    }
+    else{
+        var data = await response.json();
+
+        // console.log(data)
+    
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML= Math.round(data.main.temp )+ "° C";
+        document.querySelector(".humidity").innerHTML= data.main.humidity +"%";
+        document.querySelector(".wind").innerHTML= data.wind.speed + "Km/h";
+    
+        if(data.weather[0].main== "Clouds"){
+            weatherIcon.src = "./images/cloudy.png";
+        }
+        else if(data.weather[0].main == "rainy"){
+            weatherIcon.src= "./images/rainy.png";
+        }
+        else if(data.weather[0].main == "Sunny"){
+            weatherIcon.src= "./images/sunny.png";
+        }
+        else if(data.weather[0].main == "Rainy"){
+            weatherIcon.src= "./images (1).png";
+        }
+        document.querySelector(".weather").style.display="block"
+        document.querySelector(".error").style.display="none"
+    }
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+})
